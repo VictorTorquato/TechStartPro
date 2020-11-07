defa05a96a4cdfe26b00ae8424d03364f6d8320c
unknown <victortorquatob@gmail.com>
2020-11-07 11:33:11 -0300

    Base para CLI criada
    Dependencias da CLI instaladas

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
+const figlet = require('figlet');
+
+var inquirer = require('inquirer');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const app = express();
 
@@ -8,19 +12,42 @@ app.use(routes);
 
 app.listen(3333);
 
-// console.log('Select the categories of this product in the following model: id1 id2 id3 ...\nAvailable categories:\n');
+let xhr = new XMLHttpRequest();
 
-// console.log(await connection('category').select('*'));
 
-//var readline = require('readline');
+// xhr.open('GET', "http://localhost:3333/category", true);
+// xhr.send();
 
-//var reader = readline.createInterface({
-    //input: process.stdin,
-    //output: process.stdout
-//});
+// xhr.onreadystatechange = processRequest;
 
-// reader.question("\nCategories: ", await function(answer) {
-//     categories = answer;
-//     console.log(categories);
-//     reader.close();
-// });
+// function processRequest(e) {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let response = xhr.responseText;
+//         console.log(response);
+//     }
+// }
+
+console.clear();
+
+console.log(figlet.textSync('API', { horizontalLayout: 'full' }));
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        name: 'option',
+        type: 'input',
+        message: 'Enter the option:',
+    }
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
